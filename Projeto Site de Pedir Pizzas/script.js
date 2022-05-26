3f856d752eadb29b089750cbd60b4df5cdf5fa2c
Ana Lidia Lisboa de Andrade <analidialisboadeand@gmail.com>
2022-05-26 15:27:51 -0300

    1

---
+++ Projeto Site de Pedir Pizzas/script.js
@@ -0,0 +1,13 @@
+pizzaJson.map((item,index) =>{
+
+  let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
+
+  pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+  pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = 'R$ '+item.price.toFixed(2);
+  
+  document.querySelector('.pizza-area').append( pizzaItem );
+
+});
+
