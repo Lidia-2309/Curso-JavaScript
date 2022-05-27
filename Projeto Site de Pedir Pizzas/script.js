ae46bdebab3d30e3c607f6bbe172e5d86e2403b2
Ana Lidia Lisboa de Andrade <analidialisboadeand@gmail.com>
2022-05-27 11:54:53 -0300

    1

--- Projeto Site de Pedir Pizzas/script.js
+++ Projeto Site de Pedir Pizzas/script.js
@@ -1,13 +1,75 @@
+let modalQt = 1;
+
 pizzaJson.map((item,index) =>{
 
   let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
 
+  pizzaItem.setAttribute('data-key', index);
   pizzaItem.querySelector('.pizza-item--img img').src = item.img;
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
   pizzaItem.querySelector('.pizza-item--price').innerHTML = 'R$ '+item.price.toFixed(2);
   
-  document.querySelector('.pizza-area').append( pizzaItem );
+  pizzaItem.querySelector('a').addEventListener('click', 
+  (
+    eventoDeClique)=>{
+      eventoDeClique.preventDefault();
 
+      let key = eventoDeClique.target.closest('.pizza-item').getAttribute('data-key');
+      modalQt = 1;
+
+      document.querySelector('.pizzaBig img').src = pizzaJson[key].img;
+      document.querySelector('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+      document.querySelector('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+      document.querySelector('.pizzaInfo--actualPrice').innerHTML = 'R$ '+item.price.toFixed(2);
+      document.querySelector('.pizzaInfo--size.selected').classList.remove('selected');
+      document.querySelectorAll('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+        if(sizeIndex == 2){
+          size.classList.add('selected');
+        }
+        size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+      });
+    
+
+      document.querySelector('.pizzaWindowArea').style.opacity = 0;
+      document.querySelector('.pizzaWindowArea').style.display = 'flex';
+      setTimeout(() =>{
+      document.querySelector('.pizzaWindowArea').style.opacity = 1;
+      },200);
+    });
+
+  document.querySelector('.pizza-area').append(pizzaItem);
 });
 
+function closeModal(){
+  document.querySelector('.pizzaWindowArea').style.opacity = 0;
+  setTimeout(() => {
+    document.querySelector('.pizzaWindowArea').style.display = 'none';
+  },500);
+}
+
+document.querySelectorAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+  item.addEventListener('click',closeModal);
+});
+
+document.querySelector('.pizzaInfo--qtmenos').addEventListener('click', () =>{
+  if(modalQt > 1){
+    modalQt--;
+    document.querySelector('.pizzaInfo--qt').innerHTML = modalQt;
+  }
+});
+
+document.querySelector('.pizzaInfo--qtmais').addEventListener('click', () =>{
+    modalQt++;
+    document.querySelector('.pizzaInfo--qt').innerHTML = modalQt;
+  
+});
+
+
+
+document.querySelectorAll('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+  size.addEventListener('click', (eventoDeClique) =>{
+    document.querySelector('.pizzaInfo--size.selected').classList.remove('selected');
+    size.classList.add('selected');
+  });
+});
