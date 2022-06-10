20fda59aa6b6bdaa1d207d0e0b79689ff3c00c6b
Ana Lidia Lisboa de Andrade <analidialisboadeand@gmail.com>
2022-06-10 11:45:23 -0300

    1

--- Projetos - PARTE 2/Urna Eletrônica/script.js
+++ Projetos - PARTE 2/Urna Eletrônica/script.js
@@ -1,6 +1,6 @@
 // Definições das variáveis que usam o código do HTML
 
-let seuVotopara = document.querySelector('.d-1-1 span');
+let seuVotoPara = document.querySelector('.d-1-1 span');
 let cargo = document.querySelector('.d-1-2 span');
 let descricao = document.querySelector('.d-1-4');
 let aviso = document.querySelector('.d-2');
@@ -10,14 +10,19 @@ let numeros = document.querySelector(".d-1-3");
 
 let etapaAtual = 0;
 let numero = '';
+let VOTObranco = false;
+let votoNulo = false;
+let votos = [];
 // Funções
 
 function comecarEtapa(){
   let etapa = etapas[etapaAtual];
 
   let numeroHtml = '';
+  numero = '';
+  VOTObranco = false;
 
-  for(let i=0; i<etapa.numeros;i++){
+  for(let i=0; i < etapa.numeros;i++){
     if(i === 0){
       numeroHtml += '<div class="numero pisca"></div>';
     }else{
@@ -26,15 +31,46 @@ function comecarEtapa(){
     
   }
 
-  seuVotopara.style.display ='none';
+  seuVotoPara.style.display ='none';
   cargo.innerHTML = etapa.titulo;
   descricao.innerHTML = '';
   aviso.style.display = 'none';
   lateral.innerHTML = '';
   numeros.innerHTML = numeroHtml;
 }
+
 function atualizaInterface(){
-    alert('Finalizou digitação');
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=>{
+      if(item.numero === numero){
+        return true;
+      }
+      else{
+        return false;
+      }
+    });
+
+    if(candidato.length > 0){
+      candidato = candidato[0];
+      seuVotoPara.style.display = 'block';
+      aviso.style.display = 'block';
+      descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+      let fotosHtml = '';
+      for(let i in candidato.fotos){
+        if(candidato.fotos[i].small){
+          fotosHtml += `<div class="d-1-image small"><img src="images/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda}  <Prefeito</div>`;
+        }else{
+          fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda}  <Prefeito</div>`;
+        }
+        
+         }
+      lateral.innerHTML = fotosHtml;
+    }else{
+      seuVotoPara.style.display = 'block';
+      aviso.style.display = 'block';
+      descricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>';
+      votoNulo = true;
+    }
 }
 
 function clicou(n) {
@@ -53,15 +89,57 @@ function clicou(n) {
 }
 
 function branco(){
-  alert("Clicou em BRANCO");
+  if(numero === ''){
+    VOTObranco = true;
+    seuVotoPara.style.display = 'block';
+    aviso.style.display = 'block';
+    numeros.innerHTML = '';
+    descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
+  }
 }
 
 function corrige(){
-  alert("Clicou em CORRIGE");
+  comecarEtapa();
 }
 
 function confirma(){
-  alert("Clicou em CONFIRMA");
+  let etapa = etapas[etapaAtual];
+  let votoConfirmado = false;
+
+  if(votoNulo == true){
+    votoConfirmado = true;
+    votos.push({
+      etapa: etapas[etapaAtual].titulo,
+      voto:'nulo'
+    });
+    votoNulo = false;
+  }
+
+  else if(VOTObranco === true){
+    votoConfirmado = true;
+    votos.push({
+      etapa: etapas[etapaAtual].titulo,
+      voto:'branco'
+    });
+  }
+  
+  else if(numero.length === etapa.numeros){
+      votoConfirmado = true;
+      votos.push({
+        etapa: etapas[etapaAtual].titulo,
+        voto:numero
+      });
+  }
+
+  if(votoConfirmado){
+    etapaAtual++;
+    if((etapas)[etapaAtual] !== undefined){
+      comecarEtapa();
+    }else{
+      document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM</div>';
+      console.log(votos);
+    }
+  }
 }
 
 comecarEtapa();
